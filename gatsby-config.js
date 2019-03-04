16736ca5be0c22a269d09743b5f65856df0dd040
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2019-03-03 22:38:50 -0800

    Adds complementary plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mandacaru Technologies`,
+    description: `Mandacaru Technologies`,
+    author: `@pragmaticivan`,
   },
   plugins: [
     {
@@ -19,7 +19,54 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mandacaru Technologies`,
+        short_name: `Mandacaru Technologies`,
+        start_url: `/`,
+        background_color: `#FFFFFF`,
+        theme_color: `#5C4B77`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `static/favicon.png`, // This path is relative to the root of the site.
+        include_favicon: true, // Include favicon
+      },
+    },
+    `gatsby-plugin-netlify`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#43375b`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./static/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-135546199-1",
+      },
+    },
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
